66bc228f424a1543e5981eba140ddafcf6f3c791
safsafisalhdin786-7011 <safsafisalhdin786@gmail.com>
2026-08-27 00:35:50 +0100

    تحديث إعدادات vite والتجميع

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  build: {
+    chunkSizeWarningLimit: 1600, // رفع الحد الأدنى للتحذير إلى 1600KB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // تجميع مكتبات node_modules في ملف منفصل
+          }
+        },
+      },
+    },
+  },
+});
